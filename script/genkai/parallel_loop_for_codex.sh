2517416874d4c0a02907d46fa2d7c7555390ca91
forest-tech <m.mori.029.030@gmail.com>
2026-09-09 15:34:08 +0900

    change: NUM_ROUND = 2

--- script/genkai/parallel_loop_for_codex.sh
+++ script/genkai/parallel_loop_for_codex.sh
@@ -15,7 +15,7 @@ PRIMARY_METRIC=val_loss
 IMAGE="${IMAGE:-/home/pj24001974/ku50001532/nlp-singularity/nlp-singularity.sif}"
 WORKDIR="${WORKDIR:-/home/pj24001974/ku50001532/projects/autoresearch}"
 WORKER_SCRIPT="${WORKDIR}/script/genkai/worker.sh"
-NUM_ROUNDS="${NUM_ROUNDS:-10}"
+NUM_ROUNDS="${NUM_ROUNDS:-2}"
 NUM_WORKERS="${NUM_WORKERS:-4}"
 
 PRIMARY_METRIC="${PRIMARY_METRIC:-val_bpb}"
