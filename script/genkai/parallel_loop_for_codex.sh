02652caee3894fa26e43a450c746f16c75ba4e9b
forest-tech <m.mori.029.030@gmail.com>
2026-09-10 17:54:46 +0900

    fix: suit for exp setting.

--- script/genkai/parallel_loop_for_codex.sh
+++ script/genkai/parallel_loop_for_codex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PJM -L rscgrp=b-batch
 #PJM -L node=1
-#PJM -L elapse=02:00:00
+#PJM -L elapse=00:29:00
 #PJM -L jobenv=singularity
 #PJM -j
 
@@ -12,7 +12,8 @@ module load singularity-ce
 # =========================
 # custom
 # =========================
-WORKDIR="/home/pj24001974/ku50001532/projects/autoresearch-history-1"
+WORKDIR="/home/pj24001974/ku50001532/projects/autoresearch-history-1-test"
+NUM_ROUNDS="2"
 HISTORY_MODE="recent"
 HISTORY_LIMIT="1"
 # -------------------------
