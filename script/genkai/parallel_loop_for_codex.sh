ef25eb1c60e5f1499f67e6f62a9714d412dcb897
forest-tech <m.mori.029.030@gmail.com>
2026-09-10 18:43:39 +0900

    fix: usage of uv

--- script/genkai/parallel_loop_for_codex.sh
+++ script/genkai/parallel_loop_for_codex.sh
@@ -49,7 +49,7 @@ experiment_tool() {
         --bind "${WORKDIR}:${WORKDIR}" \
         --pwd "${WORKDIR}" \
         "${IMAGE}" \
-        uv run "${EXPERIMENT_TOOL}" "$@"
+        bash -lc "uv run '${EXPERIMENT_TOOL}' '$@'"
 }
 
 declare -a ACTIVE_WORKTREES=()
