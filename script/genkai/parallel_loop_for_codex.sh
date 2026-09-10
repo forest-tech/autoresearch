9d17bbf47f84e187c33f516ad032f5c3060926e7
forest-tech <m.mori.029.030@gmail.com>
2026-09-11 07:23:06 +0900

    fix: preserve experiment_tool arguments passed through bash -lc

--- script/genkai/parallel_loop_for_codex.sh
+++ script/genkai/parallel_loop_for_codex.sh
@@ -47,7 +47,7 @@ experiment_tool() {
         --bind "${WORKDIR}:${WORKDIR}" \
         --pwd "${WORKDIR}" \
         "${IMAGE}" \
-        bash -lc "uv run '${EXPERIMENT_TOOL}' '$@'"
+        bash -lc 'uv run "$@"' _ "${EXPERIMENT_TOOL}" "$@"
 }
 
 declare -a ACTIVE_WORKTREES=()
