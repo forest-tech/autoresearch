9c1ca450f42773b1ee906d08aa621030e0566325
forest-tech <m.mori.029.030@gmail.com>
2026-09-10 16:29:02 +0900

    change: workdir

--- script/genkai/parallel_loop_for_codex.sh
+++ script/genkai/parallel_loop_for_codex.sh
@@ -12,7 +12,7 @@ module load singularity-ce
 # =========================
 # custom
 # =========================
-
+WORKDIR="/home/pj24001974/ku50001532/projects/autoresearch-history-1"
 # -------------------------
 IMAGE="${IMAGE:-/home/pj24001974/ku50001532/nlp-singularity/nlp-singularity.sif}"
 WORKDIR="${WORKDIR:-/home/pj24001974/ku50001532/projects/autoresearch}"
